2efb5954f8acce71a29295a9cb69f8b79d9d6d31
besukes <besukesfn@gmail.com>
2026-06-08 12:23:16 +0100

    en_passant changes

--- chess_engine/en_passant.c
+++ chess_engine/en_passant.c
@@ -4,28 +4,41 @@
 
 void update_en_passant(GameStruct * game){
     CorPiece turno = game->turnoJogador;
-    if(pawnFirstRank(game->pieceCoords,turno)){
-        game->estadoJogo.enpassant = (turno==brancas) ? (game->pieceCoords >> 8) : (game->pieceCoords << 8);
+    if(pawnFirstRank(game->pieceCoords,turno) && game->pieceSelecionada == Pawn){
+        game->estadoJogo.enpassant = (turno==brancas) ? (game->pieceCoords << 8) : (game->pieceCoords >> 8);
     }
+    else game->estadoJogo.enpassant = 0;
 }
 
 
 
 Boolean can_en_passant(GameStruct * game , uint64_bit drop,CorPiece cor){
+    if(game->pieceSelecionada != Pawn) return 0;
     int pos_tab = posTabuleiro(drop);
     if(cor==brancas){
-       Boolean is_7_line = 48<=pos_tab && pos_tab < 56;
-       return (is_7_line && ((game->estadoJogo.enpassant & drop) != 0));
+       Boolean is_6_line = 40<=pos_tab && pos_tab < 48;
+       return (is_6_line && ((game->estadoJogo.enpassant & drop) != 0));
     }
     else{
-        Boolean is_1_line = 8<=pos_tab && pos_tab < 16;
-        return (is_1_line && ((game->estadoJogo.enpassant & drop) != 0));
+        Boolean is_3_line = 16<=pos_tab && pos_tab < 24;
+        return (is_3_line && ((game->estadoJogo.enpassant & drop) != 0));
     }
 }
 
 
 void enpassant_move(GameStruct * game , uint64_bit * cor_oposta , uint64_bit * mesma_cor,ShiftFunction ep_shift){
-    (*cor_oposta) = ((*cor_oposta & ~ep_shift(game->estadoJogo.enpassant,8)));
+    uint64_bit peao_removido = ep_shift(game->estadoJogo.enpassant,8);
+    (*cor_oposta) &= ~peao_removido;
     (*mesma_cor) = ((*mesma_cor & ~game->pieceCoords) | game->estadoJogo.enpassant);
     game->estadoJogo.bitboard_todas_pieces = *mesma_cor | *cor_oposta;
+
+    CorPiece turno = game->turnoJogador , op = (turno==brancas) ? pretas : brancas;
+    uint64_bit peoes_turno = game->estadoJogo.tabuleirojogo[turno][0],
+               peoes_opostos = game->estadoJogo.tabuleirojogo[op][0];
+    peoes_turno = (peoes_turno & ~game->pieceCoords) | game->estadoJogo.enpassant;
+    peoes_opostos &= ~peao_removido;
+
+    game->estadoJogo.tabuleirojogo[turno][0] = peoes_turno;
+    game->estadoJogo.tabuleirojogo[op][0] = peoes_opostos;
+    game->estadoJogo.enpassant = 0;
 }
