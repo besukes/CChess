5426d8c264e92c5094b8cfb2bd0eea6933024f1b
besukes <besukesfn@gmail.com>
2026-06-08 13:22:20 +0100

    quick en passannt fix

--- chess_engine/en_passant.c
+++ chess_engine/en_passant.c
@@ -14,14 +14,16 @@ void update_en_passant(GameStruct * game){
 
 Boolean can_en_passant(GameStruct * game , uint64_bit drop,CorPiece cor){
     if(game->pieceSelecionada != Pawn) return 0;
-    int pos_tab = posTabuleiro(drop);
+    int pos_tab_drop = posTabuleiro(drop) , pos_tab_piece = posTabuleiro(game->pieceCoords);
     if(cor==brancas){
-       Boolean is_6_line = 40<=pos_tab && pos_tab < 48;
-       return (is_6_line && ((game->estadoJogo.enpassant & drop) != 0));
+       Boolean is_6_line = 40<=pos_tab_drop && pos_tab_drop < 48 ,
+               pawn_in_pos = 32<=pos_tab_piece && pos_tab_piece < 40;
+       return (is_6_line && pawn_in_pos && ((game->estadoJogo.enpassant & drop) != 0));
     }
     else{
-        Boolean is_3_line = 16<=pos_tab && pos_tab < 24;
-        return (is_3_line && ((game->estadoJogo.enpassant & drop) != 0));
+        Boolean is_3_line = 16<=pos_tab_drop && pos_tab_drop < 24,
+                pawn_in_pos = pawn_in_pos = 24<=pos_tab_piece && pos_tab_piece < 32;
+        return (is_3_line && pawn_in_pos && ((game->estadoJogo.enpassant & drop) != 0));
     }
 }
 
