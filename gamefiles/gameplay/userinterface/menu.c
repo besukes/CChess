fe059114edcd4f6dffc62b4a5cdf216058caeff1
besukes <besukesfn@gmail.com>
2026-06-16 15:13:18 +0100

    minor fixes

--- gamefiles/gameplay/userinterface/menu.c
+++ gamefiles/gameplay/userinterface/menu.c
@@ -18,7 +18,7 @@ void desenhaButtons(CChessSettings * settings,SDL_Event event){
 }
 
 
-void dsenhaMisc(CChessSettings * settings,SDL_Event event){
+void desenhaMisc(CChessSettings * settings,SDL_Event event){
     SDL_Rect fundo = {0,0,1920,1080};
     SDL_Rect shop = {1790,30,100,100};
     SDL_Rect cchess = {490,(-60),900,500};
@@ -26,16 +26,15 @@ void dsenhaMisc(CChessSettings * settings,SDL_Event event){
     SDL_RenderCopy(settings->gameRenderer,settings->textures.miscTextures[0],NULL,&fundo);
     SDL_RenderCopy(settings->gameRenderer,settings->textures.miscTextures[3],NULL,&cchess);
     SDL_RenderCopy(settings->gameRenderer,settings->textures.miscTextures[1],NULL,&shop);
-
 }
 
 
 void desenhaInterfaceMenu(CChessSettings * settings,SDL_Event event){
     SDL_SetRenderDrawColor(settings->gameRenderer, 0, 0, 0, 180); 
     SDL_SetRenderDrawBlendMode(settings->gameRenderer, SDL_BLENDMODE_BLEND);
+
+    desenhaMisc(settings,event);
     SDL_Rect menuOptions = {660,380,600,670};
     SDL_RenderFillRect(settings->gameRenderer, &menuOptions);
-
     desenhaButtons(settings,event);
-    desenhaMisc(settings,event);
 }
